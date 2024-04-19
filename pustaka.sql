9fab1d9ffab988861ea917fdc1780fb075e95d59
winklesen <samuelbernard147@gmail.com>
2024-04-19 10:48:40 +0700

    :fire: Add CI3

---
+++ pustaka.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2023 at 04:54 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pustaka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` int(11) NOT NULL,
+  `judul_buku` varchar(128) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `pengarang` varchar(64) NOT NULL,
+  `penerbit` varchar(64) NOT NULL,
+  `tahun_terbit` year(4) NOT NULL,
+  `isbn` varchar(64) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `dipinjam` int(11) NOT NULL,
+  `dibooking` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `dipinjam`, `dibooking`, `image`) VALUES
+(7, 'Nmaaaaaa', 6, 'pengara', 'neeeer', '2021', '12121', 1919, 0, 0, 'img1701179640.jpeg'),
+(8, 'Tes 299', 4, 'Pengarang 29', 'Penerbit 292', '2019', '29292929', 29, 0, 0, 'img1701225180.png'),
+(9, 'adad', 3, 'aada', 'adada', '2022', '121212121', 12, 0, 0, 'img1701225414.jpeg'),
+(10, 'adad', 7, 'adada', 'adada', '0000', '121212121', 13, 0, 0, ''),
+(12, 'adad', 3, 'ada', 'ada', '1999', '121212121', 4, 0, 0, 'img1701225375.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(5, 'Komunikasi'),
+(6, 'Hukum'),
+(7, 'Agama'),
+(8, 'Populer'),
+(10, 'Komik'),
+(12, 'Hobby'),
+(13, 'Komik'),
+(14, 'Sains'),
+(15, 'Bahasa'),
+(16, 'GANJAR'),
+(17, 'Tesssss');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(5) NOT NULL,
+  `menu` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'administrator'),
+(2, 'member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  `tanggal_input` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `email`, `image`, `password`, `role_id`, `is_active`, `tanggal_input`) VALUES
+(3, 'Samuel', 'User@gmail.com', 'pro1701175027.png', '$2y$10$844PrVdej1Mw2pCzZ7c5mOR6izxxa3t0yq3rzoAH2fdq30SvlYhbe', 2, 1, 1698050198),
+(4, 'Samuel', 'Admin@gmail.com', 'pro1701778439.jpg', '$2y$10$844PrVdej1Mw2pCzZ7c5mOR6izxxa3t0yq3rzoAH2fdq30SvlYhbe', 1, 1, 1698050198);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.rst
@@ -0,0 +1,57 @@
+###################
+Pustaka Booking
+###################
+
+Nama    : Samuel Bernard Jeffersen
+NIM     : 19220735
+Kelas   : 19.3D.01
+
+*******************
+# Daftar Tugas
+1. [Tugas 1 ](#tugas1)
+2. [Tugas 2 ](#tugas2)
+3. [Tugas 3 ](#tugas3)
+4. [Tugas 3+](#tugas3+)
+5. [Tugas 4 ](#tugas5)
+
+
+*******************
+## tugas1
+Membuat Git WP2_NamaMahasiswa
+*******************
+[Link](https://google.com)
+
+*******************
+## tugas2
+Pembuatan file controllers "Contoh1.php" 
+*******************
+
+[Link](https://google.com)
+
+*******************
+## tugas3
+Pembuatan file konsep MVC
+*******************
+
+[Link](https://google.com)
+
+*******************
+## tugas3+
+Pembuatan konsep MVC "Form Data Mahasiswa"
+*******************
+
+[Link](https://google.com)
+
+*******************
+## tugas4
+Membuat Template Web Sederhana
+*******************
+
+[Link](https://google.com)
+
+*******************
+## tugas5
+MEMBUAT Form Validasi Pada Codeigniter
+*******************
+
+[Link](https://google.com)
