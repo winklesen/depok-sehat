1bec2d2ecf05135d9f45402d37c7ed29bc254cbe
winklesen <samuelbernard147@gmail.com>
2024-05-05 15:12:06 +0700

    :zap: Tidy up code

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -2,13 +2,11 @@
 function cek_login()
 {
     $ci = get_instance();
-    if (!$ci->session->userdata('email')) 
-    {
+    if (!$ci->session->userdata('email')) {
         $ci->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login!! </div>');
 
         // Petugas = 1, Admin  = 2
-        if ($ci->session->userdata('role_id') == 1 || $ci->session->userdata('role_id') == 2) 
-        {
+        if ($ci->session->userdata('role_id') == 1 || $ci->session->userdata('role_id') == 2) {
             redirect('autentifikasi');
         } else {
             redirect('home');
@@ -22,8 +20,7 @@ function cek_user()
 {
     $ci = get_instance();
     $role_id = $ci->session->userdata('role_id');
-    if (!$role_id == 1 && !$role_id == 2 ) 
-    {
+    if (!$role_id == 1 && !$role_id == 2) {
         $ci->session->set_flashdata('pesan', '<div class="alert alertdanger" role="alert">Akses tidak diizinkan </div>');
         redirect('home');
     }
