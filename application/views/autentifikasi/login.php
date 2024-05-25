f0603c64d559a0ab15b63a47cb87c375e7d265d4
LeGamcisco <ahmdrfzn505@gmail.com>
2024-05-25 14:51:43 +0700

    hapus regis

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -19,8 +19,8 @@
                   
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login ke Akun Anda</h5>
-                    <!-- <?= $this->session->flashdata('pesan'); ?> -->
                     <p class="text-center small">Masukkan Email dan Password untuk Login</p>
+                    <?= $this->session->flashdata('pesan'); ?>
                   </div>
                   <form class="row g-3" method="post" action="<?= base_url('autentifikasi'); ?>">
 
@@ -47,11 +47,8 @@
                         <label class="form-check-label" for="rememberMe">Remember me</label>
                       </div>
                     </div>
-                    <div class="col-12">
+                    <div class="col-12 mb-4">
                       <button class="btn btn-primary w-100" type="submit">Login</button>
-                    </div>
-                    <div class="col-12">
-                      <p class="small mb-0">Belum Memiliki Akun? <a href="<?= base_url('autentifikasi/registrasi');?>">Registrasi Akun</a></p>
                     </div>
                   </form>
 
